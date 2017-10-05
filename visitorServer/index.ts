ab0a7d81561723abf3bf98cc4d5f6eb7ac96b4bb
Christina Karali <christinakarali@christinas-mbp.groningen.nl.ibm.com>
2017-10-05 16:54:44 +0200

    Before DB shits

--- visitorServer/index.ts
+++ visitorServer/index.ts
@@ -6,10 +6,16 @@ import { Express } from "express";
 import { MyDatabase } from "./database";
 import { Employee } from "./models/employee";
 import { Visit } from "./models/visit";
+import * as bodyParser from "body-parser";
 
 
 let app: Express = express();
 let db: MyDatabase = new MyDatabase();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/api/employee', function (req, res) {
     db.selectAll()
@@ -18,13 +24,14 @@ app.get('/api/employee', function (req, res) {
 })
 
 app.get('/api/visit', function (req, res) {
-    db.postVisit("fasbjfasfsakjnfkjsan","jac@jac.it",new Date())
-        .then( () => { console.log("post is successful") } )
-        .catch( (err: Error) => { console.log("ERROR: ", err) } );
+    //db.postVisit("fasbjfasfsakjnfkjsan","jac@jac.it",new Date())
+      //  .then( () => { console.log("post is successful") } )
+        //.catch( (err: Error) => { console.log("ERROR: ", err) } );
     db.getAllVisits()
         .then( (rows: Visit[]) => { res.send(rows); })
         .catch( (err) => { res.send("ERROR") });
 })
+
 
 app.post('/api/visit', function (req, res) {
     let guest = req.body.guest;
